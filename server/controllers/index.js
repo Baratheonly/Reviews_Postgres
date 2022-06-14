7fe7bee7c6aaab19a7572915e35251ad70b141a6
Aaron Bowers <bowersaaronjames@gmail.com>
2022-06-14 14:39:39 -0700

    almost complete getReviews query. need to complete characteristics and characteristic_reviews tables and create join statements

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,18 +3,18 @@ const pool = require('../db/reviewsPostgres.js');
 module.exports = {
 
   getReviews: (req, res) => {
-    console.log('Getting reviews for product_id:', req.query.product_id);
+    let page = req.query.page || 1;
+    let count = req.query.count || 5;
+    let sort = req.query.sort || 'relevant';
+
+    if (sort === 'helpful') {
+      sort = 'helpfulness';
+    } else {
+      sort = 'date'
+    }
+
     let query = `
-      SELECT
-      review_id,
-      rating,
-      summary,
-      recommend,
-      response,
-      body,
-      date,
-      reviewer_name,
-      helpfulness,
+      SELECT review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness,
       (
         SELECT
           COALESCE (
@@ -31,19 +31,22 @@ module.exports = {
       FROM reviews
       WHERE product_id=$1 AND reviews.reported = false
       GROUP BY reviews.review_id
-      ORDER BY helpfulness DESC;
-    `
+      ORDER BY ${sort} DESC
+      LIMIT ${count}
+      OFFSET ${count * (page - 1)}
+    ;`
     pool.query(query, [req.query.product_id])
       .then(data => {
         let reviews = {
           product_id: req.query.product_id,
+          page: page,
+          count: count,
           results: data.rows
         }
-        // console.log(reviews);
         res.status(200).send(reviews);
       })
       .catch(err => {
-        // console.error(err);
+        console.error('problem getting reviews -->', err)
         res.status(400).send(err);
       })
   },
@@ -53,7 +56,39 @@ module.exports = {
   },
 
   addReview: (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
+    let timeAdded = new Date().toISOString();
+    let body = Object.values(req.body);
+    let {photos} = req.body;
+
+    let query = `
+      INSERT INTO reviews (
+        product_id, rating, summary, body, recommend, reviewer_name, reviewer_email, date
+      )
+      VALUES (
+        $1, $2, $3, $4, $5, $6, $7, $8
+      )
+      RETURNING review_id
+    ;`
+    pool.query(query, [...body.slice(0, body.length - 1), timeAdded])
+      .then(data => {
+        res.status(200).send(`review added`)
+        return data.rows[0].review_id;
+      })
+      .then(review_id => {
+        console.log('hey, look at this review_id:', review_id);
+
+        let photosQuery = `
+          INSERT INTO photos(review_id, url)
+          SELECT $1, UNNEST($2::text[])
+          RETURNING *
+        ;`
+        pool.query(photosQuery, [review_id, photos])
+          .then(data => console.log(data.rows[0]))
+          .catch(err => console.error('error adding photos -->', err))
+      })
+      .catch(err => console.error('couldn\'t add photos -->', err));
+
   },
 
   addHelpful: (req, res) => {
@@ -65,7 +100,7 @@ module.exports = {
     `
 
     pool.query(query, [req.params.review_id])
-      .then(data => res.status(200).send(`review marked as helpful`))
+      .then(() => res.status(200).send(`review marked as helpful`))
       .catch(err => console.error('helpfulness error:', err));
   },
 
@@ -82,3 +117,10 @@ module.exports = {
   }
 
 }
+
+// SELECT * FROM UNNEST(
+//   ARRAY${Array(req.body.photos.length).fill(`
+//     SELECT lastval() FROM reviews
+//   `)}::int[],
+//   ARRAY$1::text[]
+// )
