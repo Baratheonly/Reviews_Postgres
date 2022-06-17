4c21439bf22b2a8d142c40ac47ddd73415912715
Aaron Bowers <bowersaaronjames@gmail.com>
2022-06-17 12:33:09 -0700

    add supertest

---
+++ Test/supertest.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('../db/app.js');
+
+describe("GET /reviews", () => {
+  test("should respond with a status code of 200 and have an id", async () => {
+    const response = await request(app).get("/reviews/?product_id=40344");
+    expect(response.statusCode).toBe(200);
+    expect(typeof response.body[0].id).toBe("number");
+    expect(typeof response.body[0].default_price).toBe("string");
+  });
+});
