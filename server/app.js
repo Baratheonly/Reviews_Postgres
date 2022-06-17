79ebf56c87c1d0b342b8d8908292f52a333be818
Aaron Bowers <bowersaaronjames@gmail.com>
2022-06-17 13:41:28 -0700

    Revert "add loader.io"
    This reverts commit 626bdf3bed3cf1730aedd8ade4e1f789bbf205f2.

--- server/app.js
+++ server/app.js
@@ -7,10 +7,6 @@ const app = express();
 app.use(express.json());
 app.use('/reviews', router);
 
-app.get(`/loaderio-f5642590d5870794bf320a1782ef1224`, (req, res) => {
-  res.send(`loaderio-f5642590d5870794bf320a1782ef1224`);
-});
-
 app.listen(process.env.EXPRESSPORT, () => {
   console.log(`Listening on Express Port ${process.env.EXPRESSPORT}`);
 })
